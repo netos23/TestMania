2a6cfd153ea4fd4ce4a2821d66bd31a16dbfb569
netos23 <nikmoroz2002@outlook.com>
2022-05-26 12:29:32 +0300

    add tests

---
+++ collection_service/src/test/kotlin/ru/fbtw/testmania/collection_service/service/AspectServiceTest.kt
@@ -0,0 +1,44 @@
+package ru.fbtw.testmania.collection_service.service
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+internal class AspectServiceTest(
+    val aspectService: AspectService
+) {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun findAspectsByCollection() {
+    }
+
+    @Test
+    fun getAspect() {
+    }
+
+    @Test
+    fun addAspect() {
+    }
+
+    @Test
+    fun editAspect() {
+    }
+
+    @Test
+    fun deleteAspect() {
+    }
+}
---
+++ collection_service/src/test/kotlin/ru/fbtw/testmania/collection_service/service/CollectionServiceTest.kt
@@ -0,0 +1,30 @@
+package ru.fbtw.testmania.collection_service.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class CollectionServiceTest {
+
+    @Test
+    fun getCollectionList() {
+    }
+
+    @Test
+    fun getCollection() {
+    }
+
+    @Test
+    fun addCollection() {
+    }
+
+    @Test
+    fun editCollection() {
+    }
+
+    @Test
+    fun deleteCollection() {
+    }
+}
